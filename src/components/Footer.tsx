373a0d2cd1c169fc6b623ad192fe8f72d39db6a3
omar lahia <omar.lahia@gmail.com>
2024-05-29 23:17:01 +0100

    feat: add X and Github profile to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
+import { VscGithubInverted } from "react-icons/vsc";
+import { BsTwitterX } from "react-icons/bs";
+
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center p-2 text-lg">
-      <p>Made With ❤️ By <a href="https://x.com/df_trainX">df_trainX</a></p>
+    <div className="flex flex-col space-y-2 justify-center items-center p-2 text-lg">
+      <p>Made With ❤️ </p>
+      <div className="flex space-x-3">
+        <a target="_blank" href="https://github.com/LahiaOmar/tokens_viewer" > <VscGithubInverted className="w-7 h-7" /></a>
+        <a target="_blank" href="https://x.com/df_trainX" > <BsTwitterX className="w-7 h-7" /></a>
+      </div>
     </div>
   )
 }
